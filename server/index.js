066645075606c67f819cfb81d1fb5942de3d6793
maestro-imjst-ad <themaestrotaufeeq@gmail.com>
2022-04-26 22:54:29 +0530

    cookie to be added

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const userRoutes = require('./routes/user')
 
 app.use(express.json())
 app.use(cors())
 app.use('/api', userRoutes)
+app.use(cookieParser('sercretkey'))
 
 mongoose.connect(
     'mongodb+srv://taufeeq:hellolove@x-data.ysudu.mongodb.net/x-database?retryWrites=true&w=majority',
--- server/routes/user.js
+++ server/routes/user.js
@@ -16,20 +16,26 @@ router.post('/add-user', async (req, res) => {
         password: userData.password,
         firstName: userData.firstName,
         lastName: userData.lastName,
+        authToken:'',
     }
     const email = userData.email
     const password = userData.password
     const found = await userModel.find({ email: email }).exec()
     if (found.length !== 0) {
         console.log("u already exists just login not signup")
+        res.send("user already exists")
     }
     else {
         try {
             const hashedPwd = await bcrypt.hash(password, salt);
+            const token = crypto.randomBytes(32).toString('hex')
             addingData.password = hashedPwd
-            console.log(addingData)
+            addingData.authToken = token
+            
             const newUser = new userModel(addingData)
             await newUser.save()
+            res.cookie('token', token)
+            res.send(newUser)
             console.log('Registration Successful!')
         }
         catch (err) {
@@ -40,16 +46,16 @@ router.post('/add-user', async (req, res) => {
 
 router.post('/login-user', async (req, res) => {
     const userData = req.body
-    console.log(userData)
     const name = userData.name
     const password = userData.password
     const user = await userModel.findOne({ email: userData.email }).exec()
     if (user && await bcrypt.compare(password, user.password)) {
-        const token = crypto.randomBytes(32).toString('hex')
-        await userModel.findOneAndUpdate({ _id: user._id }, { authToken: token })
-        console.log(token)
-        console.log("logged in successfully!")
-        res.send('logged in successfully!')
+        // const token = crypto.randomBytes(32).toString('hex')
+        user.password = ""
+        user.authToken = ""
+        console.log(user)
+        res.send(user)
+        // res.send('logged in successfully!')
     }
     else {
         console.log('invalid email and password')
