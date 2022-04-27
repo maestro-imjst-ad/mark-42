8d7a15263dd707bfd64e58e502433ab519134b56
Titan <sahilahuja1911@gmail.com>
2022-04-27 18:30:02 +0530

    cookies implemented backend

--- server/index.js
+++ server/index.js
@@ -6,9 +6,10 @@ const cookieParser = require('cookie-parser')
 const userRoutes = require('./routes/user')
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({credentials: true, origin: true}))
+app.use(cookieParser())
 app.use('/api', userRoutes)
-app.use(cookieParser('sercretkey'))
+
 
 mongoose.connect(
     'mongodb+srv://taufeeq:hellolove@x-data.ysudu.mongodb.net/x-database?retryWrites=true&w=majority',
