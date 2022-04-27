737db5f42f13c0364e7929979c79aee85b962090
maestro-imjst-ad <themaestrotaufeeq@gmail.com>
2022-04-27 21:04:57 +0530

    error solved

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -1,14 +1,17 @@
 import axios from "axios"
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
+
 
 const getUserDetails = async (data) => {
-    // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-    const userData = await axios.post('http://localhost:8000/api/login-user', data, {credentials: 'include'});
-    console.log(userData)
+    const userData = await axios.post('http://localhost:8000/api/login-user', { data },);
     return userData.data
 }
 
 const signUpNewUser = async (data) => {
-    await axios.post('http://localhost:8000/api/add-user', { data })
+    const tokenData = await axios.post('http://localhost:8000/api/add-user', data);
+    console.log(tokenData.data)
+    cookies.set('token', tokenData.data, { path: '/' });
 }
 
 export { getUserDetails, signUpNewUser }
